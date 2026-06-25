da004a4ddb7ec7583009ae3810f5646d89a30d82
ilamaran-04 <rrilamaran@gmail.com>
2026-06-25 10:19:20 +0530

    pipeline manual trigger file run_pipeline.bat added

---
+++ run_pipeline.bat
@@ -0,0 +1,4 @@
+@echo off
+cd /d C:\Users\Ilamaran\Desktop\fintech_portfolio_pipeline_project
+"C:\Users\Ilamaran\Desktop\fintech_portfolio_pipeline_project\fintech_env\Scripts\python.exe" main.py
+pause
